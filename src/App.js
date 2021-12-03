f14f8a894e7b7763a96a46d8e9e0dece1fbf1b50
lvshclt <lvshclt@gmail.com>
2021-12-03 20:51:20 +0900

    개요

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import GNB from './components/GNB';
+import JavaScript from './components/JavaScript';
+
 function App() {
   return (
-    <div className="App">
+    <div className="App container col-lg-12">
+      <div className="row">
+        <GNB />
+        <div className="body col-lg-10">
+          <JavaScript />
+          <JavaScript />
+        </div>
+      </div>
     </div>
   );
 }
